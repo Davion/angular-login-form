f605933d6eff8131e1da5408d9e5fdb8e79cfde9
Klajdi Hoxha <klainti95@gmail.com>
2021-03-29 22:32:02 +0200

    change theme color at runtime

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-
+  isTrue: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeTheme(){
+    this.isTrue = !this.isTrue;
   }
 
 }
